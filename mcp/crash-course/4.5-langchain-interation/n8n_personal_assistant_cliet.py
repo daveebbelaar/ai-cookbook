b31e79ed020829f7404ca8b6432e8bd0f1655758
Safeer Ahmad <bsee15055@itu.edu.pk>
2025-07-17 11:03:29 +0500

    Concurrency testing for n8n

--- mcp/crash-course/4.5-langchain-interation/n8n_personal_assistant_cliet.py
+++ mcp/crash-course/4.5-langchain-interation/n8n_personal_assistant_cliet.py
@@ -1,3 +1,46 @@
 from mcp.client.sse import sse_client
 from mcp import ClientSession, StdioServerParameters
+
+import requests
+from random import randint
 import asyncio
+import nest_asyncio
+
+nest_asyncio.apply()
+# Lets make concorrent requests 
+
+url = "http://localhost:5678/webhook/concurrency_testing"
+
+
+def make_request():
+
+    rand1 = randint(10, 100)
+    rand2 = randint(10, 100)
+    rand3 = randint(10, 100)
+
+    payload = {
+        "rand1": rand1,
+        "rand2": rand2,
+        "rand3": rand3
+    }
+
+    print (f"Payload: {payload}")
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        # print(f"Response: {response}")
+        return response.text
+    else:
+        print(f"Failed to get a valid response, status code: {response.status_code}")
+
+
+data = make_request()
+
+# lets make 10 requests concurrently
+async def make_concurrent_requests(num_requests: int):
+    tasks = [asyncio.to_thread(make_request) for _ in range(num_requests)]
+    results = await asyncio.gather(*tasks)
+    return results
+
+asyncio.run(make_concurrent_requests(30))
+
